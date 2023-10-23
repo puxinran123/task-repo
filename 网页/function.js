1a0f8cd72313ad25ae2d845d1f30074f7a1f1b8c
puxinran123 <146220379+puxinran123@users.noreply.github.com>
2023-10-23 16:42:43 +0800

    Add files via upload
    网页相关的在文件夹里

---
+++ 网页/function.js
@@ -0,0 +1,31 @@
+// javascript一直弄不好，我后面就没有用这个了
+/*
+var articles = [
+  {title: '文章1', content: '这是第一篇文章的内容'},
+  {title: '文章2', content: '这是第二篇文章的内容'},
+  {title: '文章3', content: '这是第三篇文章的内容'},
+  // 可以添加更多的文章
+];
+
+// 将文章添加到页面上
+articles.forEach(function(article) {
+  var div = document.createElement('div');
+  div.className = 'article';
+  div.innerHTML = '<h2>' + article.title + '</h2><p>' + article.content + '</p>';
+  document.body.appendChild(div);
+});
+*/
+// 添加搜索功能
+/*document.getElementById('searchButton').addEventListener('click', function search() {
+  var keyword = document.getElementById('searchBox').value;
+  var results = document.getElementsByClassName('article');
+  
+  for (var i = 0; i < results.length; i++) {
+      if (results[i].textContent.includes(keyword)) {
+          results[i].style.display = 'block';
+      } else {
+          results[i].style.display = 'none';
+      }
+  }
+});*/
+
